6577b5ef8efbcac3f359847f86426fece9e02947
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 03:28:10 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,42 @@
-def third_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
+def first_challenge
+   epic_tragedy = {
+      :montague =>{
+          :patriarch =>{
+              :name => "Lord Montague",
+              :age => "53"
+          },
+          :matriarch =>{
+              :name => "Lady Montague",
+              :age =>"54"
+          },
+          :hero =>{
+              :name => "Romeo",
+              :age => "15",
+              :status => "alive"
+          },
+          :hero_friends => [
+              {:name => "Benvolio", :age =>"17", :attitude => "worried"}
+              {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
+            ]
+      },
+      :capulet =>{
+          :patriarch =>{
+              :name => "Lord Capulet",
+              :age => "51"
+          },
+          :matriarch =>{
+              :name => "Lady Capulet"
+              :age => "50"
+          },
+          :heroine =>{
+              :name => "Juliet",
+              :age => "15",
+              :status => "alive"
+          },
+          :heroine_friends =>[
+              {:name => "Steven", :age =>"30", :status => "confused"},
+              {:name =>"Nurse", :age =>"44", :status =>"worried"}
+            ]
+      }
    }
-  }
-
-  
 end
